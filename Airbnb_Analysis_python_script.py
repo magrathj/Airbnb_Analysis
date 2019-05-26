4113b8db1db633d25a73b6cbd16310ba3f194445
magrathj <magrathj@tcd.ie>
2019-05-26 21:37:15 +0100

    simulation working

--- Airbnb_Analysis_python_script.py
+++ Airbnb_Analysis_python_script.py
@@ -4,17 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
 import random
-
+from scipy.stats import expon
+from scipy.stats import gamma
+from numpy import array
+import seaborn as sns
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import time
+plotly.offline.init_notebook_mode()
+sns.set(color_codes=True)
+sns.set(rc={'figure.figsize':(5,5)})
 
 #%%
 # read in the listings
-listings_df = pd.read_csv("./Data/listings.csv")
+listings_df = pd.read_csv("./Data/london_listings.csv")
 listings_df.head()
 
 
 #%%
 # read in the reviews
-reviews_df = pd.read_csv("./Data/reviews.csv")
+reviews_df = pd.read_csv("./Data/london_reviews.csv")
 reviews_df['date'] = pd.to_datetime(reviews_df['date'])
 reviews_df.head()
 
@@ -159,11 +169,6 @@ print(df.useage.median())
 
 
 
-#%%
-# distrbution of useage
-df['useage'].hist(bins=100)
-
-
 
 #%%
 # How much income was generated 
@@ -174,43 +179,94 @@ df['income'].hist(bins=100);
 print(df['income'].describe())
 
 
+#%%
+# distrbution of useage
+#df['useage'].hist(bins=100);
+ax = sns.distplot(df['useage'],
+                  kde=True,
+                  bins=100,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Exponential Distribution', ylabel='Frequency')
+
 
 
 #%%
-# monte-carlo simulation of income generated
-def get_rand_number():
+# fitting a expontial distrbution to useage
+data_expon = expon.rvs(scale=6, loc=0, size=1000)
+ax = sns.distplot(data_expon,
+                  kde=True,
+                  bins=100,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Exponential Distribution', ylabel='Frequency')
+
+
+#%%
+# fitting a gamma distrbution to useage
+data_gamma = gamma.rvs(a=20, size=10000)
+ax = sns.distplot(data_gamma,
+                  kde=True,
+                  bins=100,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Exponential Distribution', ylabel='Frequency')
+
+
+
+
+#%%
+# monte-carlo simulation functions
+def get_rand_numbers(scale=5, size=1000):
     """
-    This function generates a random number between 1 and 100
+    This function generates a random number from a gamma distribution
     Return:
     - Random number (float)
     """
-    roll = random.randint(1,100)
+    roll = expon.rvs(scale=scale, loc=0, size=size)
     roll = roll/100
     return roll
 
-def monte_carlo_sim(df, prob = 0.2, number_sim = 1000):
+def monte_carlo_sim(df, scale =4.9, number_sim = 100):
     """
-    This function generates a random number between 1 and 100
+    This function generates a monte-carlo simulation to determine the distribution of 
+    overall incomes which are possible given a specific dataframe
+    and gamma distribution
     Return:
-    - Random number (int)
+    - final_income (array of int) each value represents the sum of the income from that simulation
     """
     x = 0
     final_income = []
     while x < number_sim:  
-        income_iter = 0      
-        for index, row in df.iterrows():
-            result = get_rand_number()
-            if(result <= prob):
-                income = row['price'] * row['availability_365'] * prob
-                income_iter += income
+        sim_outputs = df['price'] * df['availability_365']
+        y = np.array(get_rand_numbers(scale = scale, size=sim_outputs.shape[0]))
+        income_iter = sum(sim_outputs.multiply(y, axis=0))	
         final_income.append(income_iter)
         x+=1 # increase the count
     return final_income
 
 
-sim_outputs = monte_carlo_sim(df, prob = 0.25, number_sim = 10)
-print(sim_outputs)
+#%%
+# monte-carlo simulation of income generated
+def plot_simulation(df, scale = 5.2, number_sim = 1000):
+    """
+    This function generates a monte-carlo simulation to determine the distribution of 
+    overall incomes which are possible given a specific dataframe
+    and gamma distribution
+    Return:
+    - final_income (array of int) each value represents the sum of the income from that simulation
+    """
+    sim_outputs = monte_carlo_sim(df=df, scale=scale, number_sim = number_sim)
+    data = array(sim_outputs)
+    plt.xlim([min(data), max(data)])
+    plt.hist(data, bins=100)
+    plt.title('Random Gaussian data (fixed bin size)')
+    plt.xlabel('variable X (bin size = 5)')
+    plt.ylabel('count')
+    plt.axvline(df['income'].sum(), color='r')
+    plt.show()
 
 
-		
+
+plot_simulation(df , scale = 5.2, number_sim = 100000)
 
--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # Airbnb Analysis
 
+## Four Questions that the analysis in the .ipynb file is trying to answer
+
+### How much income do properties in London generate in terms of income for Airbnb?
+
+### How much income could Airbnb earn if it increased its average percentage of days a property is rented for compared to the days its available for in London?
+
+### Which location generates more income for Airbnb - Cape Town, SA or London, UK?
+
+### Where should Airbnb spend its advertising budget - in Cape Town, SA or London, UK?
+
+
+
 
 
 ## Data sources
 
-T
+### London
+http://data.insideairbnb.com/united-kingdom/england/london/2019-05-05/visualisations/listings.csv
+http://data.insideairbnb.com/united-kingdom/england/london/2019-05-05/visualisations/reviews.csv
+
+### Cape-Town
+http://data.insideairbnb.com/south-africa/wc/cape-town/2019-04-18/visualisations/listings.csv
+http://data.insideairbnb.com/south-africa/wc/cape-town/2019-04-18/visualisations/reviews.csv
+
+
+
+
+
+
+## Reference
+https://www.datacamp.com/community/tutorials/probability-distributions-python
+https://pythonprogramming.net/monte-carlo-simulator-python/
